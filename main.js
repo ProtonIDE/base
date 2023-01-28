d54f5e1e186bdd4fbfad98852cb6738875366a30
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-01-28 01:15:59 +0100

    brb

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, } = require('electron');
+const { version, branch, isDevelopmentVersion } = require('./ide.json')
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -6,9 +7,19 @@ const createWindow = () => {
         height: 600,
     })
 
-    win.loadFile('pages/index.html')
+    win.loadFile('src/pages/index.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
+    if (isDevelopmentVersion) {
+        console.log('========Development Info========\nOS: ' + process.platform + '\nProton: ' + branch + ' ' + version)
+    }
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 })
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
