1c5b2980294f7577970f6729cb9562a6bac01cb7
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-01-28 15:00:33 +0100

    i have an headace

--- themes/themes.js
+++ themes/themes.js
@@ -3,17 +3,20 @@ const standardDarkTheme = require("./standardDark.json");
 const draculaTheme = require("./dracula.json");
 const sideBar = document.getElementById("sideBar");
 const themeInfo = document.getElementById("themeInfo");
+const str = document.getElementById('string')
+const variable = document.getElementById("vari");
+const comment = document.getElementById("cmnt");
+const funct = document.getElementById("func");
 contextBridge.exposeInMainWorld("theme", {
   standardDark: (font) => {
     document.body.style.backgroundColor = standardDarkTheme.backgroundColor;
-    document.body.style.color = standardDarkTheme.textColor;
     document.body.style.fontFamily = font;
     sideBar.style.backgroundColor = standardDarkTheme.darkColor;
-    themeInfo.innerText =
-      "Theme: " +
-      standardDarkTheme.name +
-      " Made by: " +
-      standardDarkTheme.author;
+    themeInfo.innerText = "Theme: " + standardDarkTheme.name + " Made by: " + standardDarkTheme.author;
+    str.style.color=standardDarkTheme.syntaxHighlight.string
+    variable.style.color=standardDarkTheme.syntaxHighlight.variable
+    comment.style.color=standardDarkTheme.syntaxHighlight.comment
+    funct.style.color=standardDarkTheme.syntaxHighlight.function
   },
   dracula: (font) => {
     document.body.style.backgroundColor = draculaTheme.backgroundColor;
